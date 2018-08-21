b9182344932586919c191717862163f03a4a3eb8
taro.yamada <poirot343@outlook.jp>
2018-08-21 13:09:41 +0000

    ititaita3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'microposts/create'
+
+  get 'microposts/destroy'
 
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
